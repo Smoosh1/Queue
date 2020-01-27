379ba55953d3bf71af2fbebbbcc58d62a6510cca
Smoosh1 <macievskyivan@gmail.com>
2020-01-28 00:19:01 +0200

    Scripts for queue
    Signed-off-by: Smoosh1 <macievskyivan@gmail.com>

---
+++ Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);   
+            queue.Enqueue(7);
+            queue.Enqueue(42);
+            queue.Enqueue(69);
+            queue.Enqueue(-17);
+            Console.WriteLine($"Тут {queue.Count} элементов.");
+
+            /*А теперь нужно эту всю херабору в мейн запихнуть 
+            var _Num = queue.Top();
+            Console.WriteLine($"Первый элемент из очереди {_Num}.");*/
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    namespace Queue
+    {
+
+        public class Queue<Number>
+        {
+            #region Инициализация, ничего больше
+            private List<Number> _number = new List<Number>();
+            public int Count => _number.Count;
+            #endregion
+            #region вставляем элемент на позицию 
+
+            public void ChoosePopNum(ref int _ChoosenNum)
+            {
+                Console.WriteLine("Выберите позицию элементика который надо вставить ");
+                _ChoosenNum = int.Parse(Console.ReadLine());
+            }
+
+            public void Push(Number _Num, ref int _ChoosenNum)
+            {
+                if (_ChoosenNum > _number.Count)
+                {
+                    Console.WriteLine("Введи нормальное число");
+                }
+                else
+                {
+                    var Last_Num = _number[_number.Count - _ChoosenNum];
+                }
+                _number.Insert(_ChoosenNum,_Num);
+            }
+
+            #endregion
+            #region смотрим на первый элемент 
+
+            public Number Top()
+            {
+                var _Num = GetFirsNumber();
+                return _Num;
+            }
+            #endregion
+            #region удаляем первый элемент
+            public void Pop(Number _Num)
+            {
+                 //_Num.RemoveAt(0);
+
+            }
+            #endregion
+            #region Ищу первый элемент
+            private Number GetFirsNumber()
+            {
+                var _FirsNum = _number.FirstOrDefault();
+
+                if (_FirsNum == null)
+                {
+                    Console.WriteLine("Очередь пуста. Нет элементов для получения.");
+                }
+                return _FirsNum;
+            }
+            #endregion
+        }
+    }
+}
